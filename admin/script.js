c1581d36558a8eb325da417ded9a370c1890ba2a
Teapotuser <inessa.chermenskaya@gmail.com>
2023-07-21 03:01:49 +0300

    Admin Panel adaptivity

--- admin/script.js
+++ admin/script.js
@@ -5,6 +5,22 @@ const body = document.querySelector('body'),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
+      // will be called whenever window size changes
+window.addEventListener('resize', function() {
+	// viewport and full window dimensions will change
+	
+	var viewport_width = window.innerWidth;
+	// var viewport_height = window.innerHeight;
+    if (viewport_width < 900){
+        if(!sidebar.classList.contains("close")){
+            sidebar.classList.add("close");
+            toggle.style.pointer="none";       
+        }
+    }
+});
+
+
+
 
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
